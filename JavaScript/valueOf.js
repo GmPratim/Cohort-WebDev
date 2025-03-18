ce861dc56b1f8728e0390397855ff0b0ee3c3542
Pratim <pratim7674ghosh@gmail.com>
2025-03-19 04:24:58 +0530

    cohort Array qus solving

--- JavaScript/valueOf.js
+++ JavaScript/valueOf.js
@@ -1,4 +1,4 @@
 const str = new Number(1);
-console.log(str); // Output: "Hello"
+console.log(str.valueOf()); // Output: "Hello"
 const obj = { name: "John" };
 console.log(obj.valueOf()); // Output: { name: "John" }
