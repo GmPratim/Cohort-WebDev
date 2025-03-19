f8974c0b5f423c61afd8ff41fa45acb53a3a7c84
Pratim <pratim7674ghosh@gmail.com>
2025-03-19 18:57:25 +0530

    objects

---
+++ JavaScript/9th_Feb.js
@@ -0,0 +1,12 @@
+// craete an object representing a type of tea with properties for name, type and caffeine content
+
+const teas = {
+  name: "Lemon tea",
+  type: "Green",
+  caffine: "low",
+};
+
+// Access and print the name and type properties of the tea object.
+
+console.log(teas.name);
+console.log(teas["type"]);
