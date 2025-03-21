2144ac3b05631c727ba99a89ad0b709568e13898
Pratim <pratim7674ghosh@gmail.com>
2025-03-21 12:35:28 +0530

    class object

--- JavaScript/class.js
+++ JavaScript/class.js
@@ -1,3 +1,28 @@
+const obj1 = {
+  fName: "Pratim",
+  lName: "Ghosh",
+  getFullname: function () {
+    if (this.lname) return `${this.fName} ${this.lName}`;
+  },
+};
+
+const obj2 = {
+  fName: "P",
+  lName: "G",
+  getFullname: function () {
+    if (this.lname) return `${this.fName} ${this.lName}`;
+  },
+};
+
+// DRY - Do not repeate yourself.
+// we r repeting in obj2 same prototypes function, but we have better approach
+
+console.log(obj1.getFullname());
+console.log(obj2.getFullname());
+
+// better approach
+// class object
+
 class Person {
   constructor(fname, lname) {
     this.fname = fname;
