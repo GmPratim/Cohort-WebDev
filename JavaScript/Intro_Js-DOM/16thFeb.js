e720d2b12e15e2735408b6137400276322ff726d
Pratim <pratim7674ghosh@gmail.com>
2025-03-22 18:07:26 +0530

    function

---
+++ JavaScript/Intro_Js-DOM/16thFeb.js
@@ -0,0 +1,34 @@
+/* 
+
+Function.prototype.describe = function () {
+  console.log(`Function name is ${this.name}`);
+};
+
+function masalaChai() {}
+function greet() {}
+
+masalaChai.describe();
+greet.describe();
+
+*/
+
+/*
+
+function appl(a, b, operation) {
+  return operation(a, b);
+}
+const result = appl(5, 4, (x, y) => x / y);
+console.log(result);
+
+*/
+
+// tiffine concept
+// Closure function
+
+function createCounter() {
+  let count = 0;
+  return function () {
+    count++;
+    return count;
+  };
+}
