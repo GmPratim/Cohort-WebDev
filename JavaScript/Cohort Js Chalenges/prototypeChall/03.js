e4855db5a9cb2598209238b34ba4a249e4acfdc3
Pratim <pratim7674ghosh@gmail.com>
2025-03-27 23:27:23 +0530

    js challenges

---
+++ JavaScript/Cohort Js Chalenges/prototypeChall/03.js
@@ -0,0 +1,22 @@
+/*
+Problem statement
+You need to create a counter constructor function that initializes a count property to The counter should have two prototype methods:
+• increment() increases the count by 1.
+• decrement() decreases the count by 1.
+Challenge
+• Implementa constructor function counter that initializes count to e
+• Attach increment() and decrement() methods tothe prototype.
+*/
+
+function Counter() {
+  this.count = 0;
+}
+// Adding increment method to the prototype
+
+Counter.prototype.increment = function () {
+  this.count++;
+};
+// Adding decrement method to the prototype
+Counter.prototype.decrement = function () {
+  this.count--;
+};
