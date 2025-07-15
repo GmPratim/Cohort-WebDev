975dd58052a5bdac7ca7292bbdc97d172fc497e0
Pratim Ghosh <onlyforgaming18p@gmail.com>
2025-07-16 00:26:09 +0530

    js

---
+++ JavaScript/22 Feb class/01_variables_datatypes.js
@@ -0,0 +1,22 @@
+let name = "pratim"
+const pi = 3.14
+
+let number = 42 // Number
+let text = "Hello, World!" // String
+let isTrue = true // Boolean
+let nothing = null // Null
+let undefinedValue = undefined // Undefined 
+let symbolVar = Symbol() // Symbol  
+
+
+
+let person = {
+    name: 'pratim',
+    age: 22,
+    isStudent: true,
+}
+
+// converting a number to string
+let str = 123
+let convertdString = String(str)
+
