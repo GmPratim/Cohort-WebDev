8ada1b0be2b3588d9553485c468bd5c9af7ba29c
Pratim Ghosh <onlyforgaming18p@gmail.com>
2025-08-12 19:14:56 +0530

    js

---
+++ js-HomeWork/start/script.js
@@ -0,0 +1,27 @@
+
+
+function updateTime (){
+    const timeEvent = document.getElementById("time")
+    const dateEvent = document.getElementById("date")
+
+    const now = new Date()
+    const hours = now.getHours() % 12 || 12
+    const minutes = now.getMinutes().toString().padStart(2, "0")
+    const seconds= now.getSeconds().toString().padStart(2, "0")
+    const ampm = now.getHours() >= 12 ? "PM" : "AM"
+
+    const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+    timeEvent.textContent = `${hours}:${minutes}:${seconds}:${ampm}`
+    dateEvent.textContent = now.toLocaleDateString(undefined, options);
+
+}
+
+setInterval(updateTime,1000)
+
+updateTime()
