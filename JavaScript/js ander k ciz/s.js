03e496ab484d28ceb0d13d77602ca14389daded2
Pratim Ghosh <onlyforgaming18p@gmail.com>
2025-08-16 21:57:18 +0530

    js

---
+++ JavaScript/js ander k ciz/s.js
@@ -0,0 +1,15 @@
+const obj = {
+    personName: "P",
+    greet: function(){
+        console.log(`Hello ${this.personName}`);
+        
+    }
+}
+
+console.log('hi');
+
+setTimeout(obj.greet.bind(obj), 5 * 1000)   // we use bind because giving object context, for setTimeout its not wrking without bind
+
+console.log('hi');
+
+
